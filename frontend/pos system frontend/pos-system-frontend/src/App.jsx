91dc878dd86e4156063c314c7508b9d72e170ff9
dvijaykumarr <dvk200507@gmail.com>
2026-03-03 22:39:02 +0530

    Added frontend - Vite + React + Tailwind + shadcn setup

---
+++ frontend/pos system frontend/pos-system-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    <>
+    
+      <Button>
+        Vijay pos system
+      </Button>
+      
+    </>
+  )
+}
+
+export default App
